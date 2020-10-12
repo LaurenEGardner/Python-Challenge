a170ee305ffe77ae850c346df6b269271da17c33
LaurenEGardner <heylo.gardner@gmail.com>
2020-10-12 15:48:53 -0700

    added requirements/goals for each file

---
+++ PyBank/main.py
@@ -0,0 +1,6 @@
+#Create script that calculates: 
+    #Total number of months
+    #net total amount of "profit/losses" over entire period
+    #average of changes in "profit/losses"
+    #greatest increase in profits (date & amount)
+    #greatest decrease in losses (date & amount)
---
+++ PyPoll/main.py
@@ -0,0 +1,5 @@
+#Create script that calculates: 
+    #Total number of votes
+    #complete list of candidates who received votes
+    #percentage of votes each candidate won
+    #winner of the election
