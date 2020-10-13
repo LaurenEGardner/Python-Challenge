4578bbec189110dd782a5a92c186a1f75d1863a9
LaurenEGardner <heylo.gardner@gmail.com>
2020-10-12 23:13:40 -0700

    Finished pybank up as much as I am able for now

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,12 +10,46 @@ import csv
 
 csvpath = os.path.join('Resources','budget_data.csv')
 
+
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
+
+    ##Variables
+    Months = 0
+    Total = 0
+    Average_Change = 0
+    Greatest_Increase_Amount = 0
+    Greatest_Increase_Month = ""
+    Greatest_Decrease_Amount = 0
+    Greatest_Decrease_Month = ""
+    Folder = {}
 
     csv_header=next(csvreader)
     print(f"CSV Header:{csv_header}")
     
+    for row in csvreader:
+        Months += 1
+        Total += int(row[1])
 
-    Months = sum(1 for row in csvreader)
-    print(Months)
+        if int(row[1]) > Greatest_Increase_Amount:
+            Greatest_Increase_Amount = int(row[1])
+            Greatest_Increase_Month = row[0]
+
+        if int(row[1])< Greatest_Decrease_Amount:
+            Greatest_Decrease_Amount = int(row[1])
+            Greatest_Decrease_Month = row[0]
+
+    #this feels wrong but my brain wont work right now to fix it
+    Average_Change = round(Total/Months)
+
+    
+    with open('output.txt', 'w') as f:
+        print("Financial Analysis", file=f)
+        print("----------------------------", file=f)
+        print("Total Months: " + str(Months), file=f)
+        print("Total: " +"$" + str(Total), file=f)
+        print("Average Change: ", "$", str(Average_Change), file=f)
+        print("Greatest Increase in Profits: ", Greatest_Increase_Month, "($", str(Greatest_Increase_Amount), ")", file=f)
+        print("Greatest Decrease in Profits: ", Greatest_Decrease_Month, "($", str(Greatest_Decrease_Amount), ")",  file=f)
+    
+       
